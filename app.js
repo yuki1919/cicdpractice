fd2cc42e3d2bc7a452096bb537ffd98ec22d1be2
yuki1919 <1071255278@qq.com>
2026-01-19 19:42:36 +0800

    feat: update app message to version 2.0

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Cloud Computing Class!');
+  res.end('Hello Cloud Computing Class! This is VERSION 2.0 - Updated on ' + new Date().toISOString().split('T')[0]);
 });
+
 server.listen(3000, () => {
   console.log('Server running on port 3000');
 });
